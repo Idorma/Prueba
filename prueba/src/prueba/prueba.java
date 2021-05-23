b47cbbbc0c6af6d99c450d2a74b156c27df2ce33
Idorma <69322475+Idorma@users.noreply.github.com>
2021-05-23 18:57:24 +0200

    Update prueba.java

--- prueba/src/prueba/prueba.java
+++ prueba/src/prueba/prueba.java
@@ -5,10 +5,11 @@ public class prueba {
 	public static void main(String[] args) {
 
 
-		System.out.println("Hoal1");
+		System.out.println("Hola1");
 		System.out.println("Hola2");
 		System.out.println("Hola ...");
 		System.out.println("Me quiero morir");
+		
 
 	}
 
